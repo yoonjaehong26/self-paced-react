4e55c29fb972a62734235c948bbb02ad63e819c2
윤재홍 <a287933@naver.com>
2025-09-26 02:49:58 +0900

    refactor(/src):index.html dom요소 리액트 컴포턴트로 재구성
    css는 전역 App.css로 임시적용함.이미지 파일 연동 안됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import HomeHeader from "./components/Header/HomeHeader.jsx";
+import RestaurantCategoryFilter from "./components/Main/RestaurantCategoryFilter.jsx";
+import RestrauantList from "./components/Main/RestrauantList.jsx";
+import RestaurantDetailModal from "./components/Aside/RestaurantDetailModal.jsx";
+import AddRestaurantModal from "./components/Aside/AddRestaurantModal.jsx";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <>
+      <header>
+        <HomeHeader />
+      </header>
+
+      <main>
+        <RestaurantCategoryFilter></RestaurantCategoryFilter>
+        <RestrauantList></RestrauantList>
+      </main>
+
+      <article>
+        <RestaurantDetailModal></RestaurantDetailModal>
+      <AddRestaurantModal></AddRestaurantModal>
+      </article>
+    </>
+  );
 }
 
 export default App;
