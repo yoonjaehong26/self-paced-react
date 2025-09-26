828959a1de3869670f31f52aa7a1966e56523021
윤재홍 <a287933@naver.com>
2025-09-26 21:06:33 +0900

    fix(eslintrc.cjs, package-lock.json, package.json):eslint버전 안정화
    eslint오류로 임시로 eslint버전을 낮게하였음
    'linebreak-style': 0: 맥북과 줄바꿈기호 호환성이 맞지 않아 임시 경고무시처리
    "react/react-in-jsx-scope": "off",:버전이 낮아 지금은 필요없지만 해당버전에서는 아직 수정 전

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,8 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    'linebreak-style': 0,
+    "react/react-in-jsx-scope": "off",
      "react/jsx-no-target-blank": "off",
     "react/prop-types": "off",
     "react-refresh/only-export-components": [
