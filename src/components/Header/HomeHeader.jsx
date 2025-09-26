d5d5da1332c3f3d8a35e8be1e873a033dab2bd0f
윤재홍 <a287933@naver.com>
2025-09-26 23:35:16 +0900

    refactor(UI):버튼, 필터 컴포넌트 분리하여 ui폴더에 할당

--- src/components/Header/HomeHeader.jsx
+++ src/components/Header/HomeHeader.jsx
@@ -1,12 +1,11 @@
- import styles from './HomeHeader.module.css'
- 
+import Button from '../UI/button';
+import styles from './HomeHeader.module.css';
+
 function HomeHeader() {
   return (
     <header className={styles.gnb}>
       <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
-      <button type="button" className={styles.gnb__button} aria-label="음식점 추가">
-        <img src="templates\add-button.png" alt="음식점 추가" />
-      </button>
+      <Button />
     </header>
   );
 }
