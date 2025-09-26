7fa73f5e597a075d2fdb42fd4caebd5d6af59be3
윤재홍 <a287933@naver.com>
2025-09-26 12:23:38 +0900

    fix(HomeHeader.jsx):HomeHeader.module.css가 적용되지 않은 부분 수정

--- src/components/Header/HomeHeader.jsx
+++ src/components/Header/HomeHeader.jsx
@@ -4,7 +4,7 @@ function HomeHeader() {
   return (
     <header className={styles.gnb}>
       <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
-      <button type="button" className="gnb__button" aria-label="음식점 추가">
+      <button type="button" className={styles.gnb__button} aria-label="음식점 추가">
         <img src="templates\add-button.png" alt="음식점 추가" />
       </button>
     </header>
