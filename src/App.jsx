d8536958892de82a8ff5a246ebd25908a12b724f
윤재홍 <a287933@naver.com>
2025-09-26 22:24:05 +0900

    feat(public/images,App.jsx,/main):usestate로 음식점 리스트 관리기능 추가
    이미지 public파일로 이동,음식점 데이터 usestate로 저장하여 동적인 음식점리스트 기능 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 import HomeHeader from './components/Header/HomeHeader';
 import RestaurantCategoryFilter from './components/Main/RestaurantCategoryFilter';
 import RestaurantList from './components/Main/RestaurantList';
 import RestaurantDetailModal from './components/Aside/RestaurantDetailModal';
 import AddRestaurantModal from './components/Aside/AddRestaurantModal';
+import restaurant from './data/restaurantsData';
 
 function App() {
+  const [filteredRestaurants] = useState(restaurant);
+
   return (
     <div className={styles.app}>
       <HomeHeader />
 
       <main>
         <RestaurantCategoryFilter />
-        <RestaurantList />
+        <RestaurantList restaurants={filteredRestaurants} />
       </main>
 
       <article>
