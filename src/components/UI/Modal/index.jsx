40b4a8d02171f71de18a79e77f39ea9f272cf298
“윤재홍” <a287933@naver.com>
2025-09-30 15:25:43 +0900

    refactor(Modal):모든 운영체제 호환성을 위하여 이미지 경고 \사용 금지

--- src/components/UI/Modal/index.jsx
+++ src/components/UI/Modal/index.jsx
@@ -1,13 +1,13 @@
 import styles from './Modal.module.css';
 
-function Modal({children}) {
+function Modal({ children }) {
   return (
-     <div className={`${styles.modal} ${styles.modalOpen}`}>
-         <div className={styles.modalBackdrop} />
-         <div className={styles.modalContainer}>
-            {children}
-         </div>   
+    <div className={`${styles.modal} ${styles.modalOpen}`}>
+      <div className={styles.modalBackdrop} />
+      <div className={styles.modalContainer}>
+        {children}
       </div>
+    </div>
   );
 }
 
